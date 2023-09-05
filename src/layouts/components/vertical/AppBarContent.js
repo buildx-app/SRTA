12ffcf84c79ddf06ebd69468f54b5c7c40fcef9d
Dhruvi Sangani <140688647+dhruvi-jspit@users.noreply.github.com>
2023-09-05 10:04:36 +0530

    removed authentication in vertical navigattion and appbar

--- src/layouts/components/vertical/AppBarContent.js
+++ src/layouts/components/vertical/AppBarContent.js
@@ -123,16 +123,14 @@ const AppBarContent = props => {
           </IconButton>
         ) : null}
         {/* {auth.user && <Autocomplete hidden={hidden} settings={settings} />} */}
-        <Typography sx={{ fontSize: '30px', fontWeight: 700 }}>Dashboard</Typography>
+        <Autocomplete hidden={hidden} settings={settings} />
       </Box>
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
-        {/* <LanguageDropdown settings={settings} saveSettings={saveSettings} />
-        <ModeToggler settings={settings} saveSettings={saveSettings} /> */}
+        <LanguageDropdown settings={settings} saveSettings={saveSettings} />
+        <ModeToggler settings={settings} saveSettings={saveSettings} />
         {/* {auth.user && ( */}
         <>
-          {/* <ShortcutsDropdown settings={settings} shortcuts={shortcuts} /> */}
-          <IconifyIcon icon={'tabler:search'} fontSize='25px' />
-
+          <ShortcutsDropdown settings={settings} shortcuts={shortcuts} />
           <NotificationDropdown settings={settings} notifications={notifications} />
           <UserDropdown settings={settings} />
         </>
