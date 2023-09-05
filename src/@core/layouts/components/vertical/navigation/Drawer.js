ab3be64e51c0b43d4dd0860bbe2cfd9e4ef21531
Dhruvi Sangani <140688647+dhruvi-jspit@users.noreply.github.com>
2023-09-05 18:28:14 +0530

    modified appbar design

--- src/@core/layouts/components/vertical/navigation/Drawer.js
+++ src/@core/layouts/components/vertical/navigation/Drawer.js
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles'
 import MuiSwipeableDrawer from '@mui/material/SwipeableDrawer'
 
 const SwipeableDrawer = styled(MuiSwipeableDrawer)({
+  border: '0px',
   overflowX: 'hidden',
   transition: 'width .25s ease-in-out',
   '& ul': {
@@ -88,12 +89,10 @@ const Drawer = props => {
       PaperProps={{
         sx: {
           backgroundColor: 'background.paper',
-          background: '#FFF',
-          boxShadow: '9px 4px 24px 0px rgba(0, 0, 0, 0.04)',
-          // ...(!hidden && skin !== 'bordered' && { boxShadow: 2 }),
+          ...(!hidden && skin == 'bordered' && { boxShadow: '9px 4px 24px 0px rgba(0, 0, 0, 0.04)' }),
           width: navCollapsed && !navHover ? collapsedNavWidth : navWidth,
-          // borderRight: theme =>
-          //   navigationBorderWidth === 0 ? 0 : `${navigationBorderWidth}px solid ${theme.palette.divider}`,
+          borderRight: theme =>
+            navigationBorderWidth === 0 ? `${navigationBorderWidth}px solid ${theme.palette.divider}` : 0,
           ...userNavMenuPaperStyle
         },
         ...navMenuProps?.PaperProps
