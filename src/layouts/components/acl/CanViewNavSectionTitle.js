9be22152c4f679c231a580c6ba915271384418b3
Dhruvi Sangani <140688647+dhruvi-jspit@users.noreply.github.com>
2023-09-05 10:04:36 +0530

    removed authentication in vertical navigattion and appbar

--- src/layouts/components/acl/CanViewNavSectionTitle.js
+++ src/layouts/components/acl/CanViewNavSectionTitle.js
@@ -1,20 +1,7 @@
-// ** React Imports
-import { useContext } from 'react'
-
-// ** Component Imports
-import { AbilityContext } from 'src/layouts/components/acl/Can'
-
 const CanViewNavSectionTitle = props => {
-  // ** Props
-  const { children, navTitle } = props
+  const { children } = props
 
-  // ** Hook
-  const ability = useContext(AbilityContext)
-  if (navTitle && navTitle.auth === false) {
-    return <>{children}</>
-  } else {
-    return ability && ability.can(navTitle?.action, navTitle?.subject) ? <>{children}</> : null
-  }
+  return <>{children}</>
 }
 
 export default CanViewNavSectionTitle
