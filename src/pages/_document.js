fe8c4cb9052349b15147b674314a21588efbc06c
Dhruvi Sangani <140688647+dhruvi-jspit@users.noreply.github.com>
2023-09-06 16:19:15 +0530

    dashboard student chart added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,12 +15,6 @@ class CustomDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          {/* <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'
-          /> */}
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
@@ -28,7 +22,7 @@ class CustomDocument extends Document {
             rel='stylesheet'
           />
           <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
-          <link rel='shortcut icon' href='/images/mobile-logo.png' />
+          <link rel='shortcut icon' href='/images-srpa/mobile-logo.png' />
         </Head>
         <body>
           <Main />
