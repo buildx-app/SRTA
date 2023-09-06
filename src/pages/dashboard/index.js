7f6e86f7df09716e6daaa2bb35e89af8d2e64e02
Dhruvi Sangani <140688647+dhruvi-jspit@users.noreply.github.com>
2023-09-06 16:19:15 +0530

    dashboard student chart added

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Grid } from '@mui/material'
+import TotalStudentChart from 'src/views/dashboard/TotalStudentChart'
 
 function index() {
-  return <div>dashboard</div>
+  return (
+    <>
+      <Grid container spacing={6}>
+        <Grid item xs={12} lg={4}>
+          <TotalStudentChart />
+        </Grid>
+        <Grid item xs={12} lg={8}>
+          <h2>second part</h2>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 export default index
