6f43426a1d4a4a782badcc53a64dd6b5a2283631
mihir07121 <mihir@jspinfotech.com>
2023-09-06 16:00:56 +0530

    table(dashboard and users)

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,7 +1,231 @@
-import React from 'react'
+import { React } from 'react'
+import { Card, CardContent, Avatar, Box, Typography, Grid } from '@mui/material'
+import { deepOrange } from '@mui/material/colors'
+import StudentTable from 'src/views/dashboard/StudentTable'
+import ExamsTable from 'src/views/dashboard/ExamsTable'
 
 function index() {
-  return <div>dashboard</div>
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card
+            className='card'
+            style={{
+              backgroundColor: '#CBF1FF',
+              border: '0px'
+            }}
+          >
+            <CardContent
+              fullwidth
+              className='card-box'
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '16px'
+              }}
+            >
+              <Avatar
+                className='card-avatar'
+                sx={{
+                  bgcolor: '#ffffff',
+                  width: '70px',
+                  height: '70px',
+                  padding: '15px',
+                  marginRight: '15PX'
+                }}
+                src='/images/avatars-new/Students.svg'
+              />
+
+              <Box
+                sx={{
+                  textAlign: 'center',
+                  flexGrow: 1,
+                  borderLeft: '1px solid #D9D9D9'
+                }}
+              >
+                <Typography
+                  variant='h6'
+                  className='card-text'
+                  sx={{
+                    lineHeight: '28px'
+                  }}
+                >
+                  Number of Students
+                </Typography>
+                <Typography variant='h6' className='card-text'>
+                  2468
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card
+            className='card'
+            style={{
+              backgroundColor: '#D8EAD1',
+              border: '0px'
+            }}
+          >
+            <CardContent
+              fullwidth
+              className='card-box'
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '16px'
+              }}
+            >
+              <Avatar
+                className='card-avatar'
+                sx={{
+                  bgcolor: '#ffffff',
+                  width: '70px',
+                  height: '70px',
+                  padding: '15px',
+                  marginRight: '15PX'
+                }}
+                src='/images/avatars-new/Teacher.svg'
+              />
+
+              <Box
+                sx={{
+                  textAlign: 'center',
+                  flexGrow: 1,
+                  borderLeft: '1px solid #D9D9D9'
+                }}
+              >
+                <Typography
+                  variant='h6'
+                  className='card-text'
+                  sx={{
+                    lineHeight: '28px'
+                  }}
+                >
+                  Number of Students
+                </Typography>
+                <Typography variant='h6' className='card-text'>
+                  2468
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card
+            className='card'
+            style={{
+              backgroundColor: '#DFE4F4',
+              border: '0px'
+            }}
+          >
+            <CardContent
+              fullwidth
+              className='card-box'
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '16px'
+              }}
+            >
+              <Avatar
+                className='card-avatar'
+                sx={{
+                  bgcolor: '#ffffff',
+                  width: '70px',
+                  height: '70px',
+                  padding: '15px',
+                  marginRight: '15PX'
+                }}
+                src='/images/avatars-new/Employee.svg'
+              />
+
+              <Box
+                sx={{
+                  textAlign: 'center',
+                  flexGrow: 1,
+                  borderLeft: '1px solid #D9D9D9'
+                }}
+              >
+                <Typography
+                  variant='h6'
+                  className='card-text'
+                  sx={{
+                    lineHeight: '28px'
+                  }}
+                >
+                  Number of Students
+                </Typography>
+                <Typography variant='h6' className='card-text'>
+                  2468
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={3}>
+          <Card
+            className='card'
+            style={{
+              backgroundColor: '#CEF3DC',
+              border: '0px'
+            }}
+          >
+            <CardContent
+              fullwidth
+              className='card-box'
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                padding: '16px'
+              }}
+            >
+              <Avatar
+                className='card-avatar'
+                sx={{
+                  bgcolor: '#ffffff',
+                  width: '70px',
+                  height: '70px',
+                  padding: '15px',
+                  marginRight: '15PX'
+                }}
+                src='/images/avatars-new/Revenue.svg'
+              />
+
+              <Box
+                sx={{
+                  textAlign: 'center',
+                  flexGrow: 1,
+                  borderLeft: '1px solid #D9D9D9'
+                }}
+              >
+                <Typography
+                  variant='h6'
+                  className='card-text'
+                  sx={{
+                    lineHeight: '28px'
+                  }}
+                >
+                  Number of Students
+                </Typography>
+                <Typography variant='h6' className='card-text'>
+                  2468
+                </Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item lg={8}>
+          <StudentTable />
+        </Grid>
+      </Grid>
+
+      <Grid item xs={8}>
+        <ExamsTable />
+      </Grid>
+    </>
+  )
 }
 
 export default index
---
+++ src/views/dashboard/StudentTable.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import { Typography, Box, IconButton } from '@mui/material'
+import Icon from 'src/@core/components/icon'
+import TableBasic from '../table/data-grid/TableBasic'
+
+const columns = [
+  {
+    flex: 0.25,
+    minWidth: 120,
+    field: 'studentname',
+    headerName: 'Student Name'
+  },
+  {
+    flex: 0.15,
+    minWidth: 100,
+    field: 'examcode',
+    headerName: 'Exam Code'
+  },
+  {
+    flex: 0.15,
+    minWidth: 120,
+    field: 'section',
+    headerName: 'Section'
+  },
+
+  {
+    flex: 0.15,
+    minWidth: 120,
+    field: 'register',
+    headerName: 'Register',
+    sortable: false,
+    renderCell: () => <ActionOptions />
+  }
+]
+const ActionOptions = () => (
+  <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <IconButton variant='outlined' sx={{ fontSize: '21px' }}>
+      <Icon icon='tabler:circle-check' />
+    </IconButton>
+  </Box>
+)
+const rows = [
+  {
+    id: 1,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 2,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 3,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 4,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 5,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 6,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 7,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 8,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 9,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  },
+  {
+    id: 10,
+    studentname: 'dfgsdf',
+    examcode: 'sdfsdf ',
+    section: 'xyx@gmail.com',
+    register: ''
+  }
+]
+
+function StudentTable() {
+  return (
+    <>
+      <Box sx={{ pt: 5, pb: 5 }}>
+        <Typography variant='h6'>Students</Typography>
+      </Box>
+      <Box sx={{ height: 550 }}>
+        <TableBasic columns={columns} rows={rows} />
+      </Box>
+    </>
+  )
+}
+
+export default StudentTable
