319200dcd45ac050e12c2f7565abb952fdebbe25
maulikjsp <maulikd@jspinfotech.com>
2023-09-07 17:11:36 +0530

    connect users api to users table

--- src/views/users/UsersTable.js
+++ src/views/users/UsersTable.js
@@ -4,8 +4,6 @@ import OptionsMenu from 'src/@core/components/option-menu'
 import OptionsMenu from 'src/@core/components/option-menu'
 import { DataGrid } from '@mui/x-data-grid'
 import Icon from 'src/@core/components/icon'
-import { Theme } from '@fullcalendar/core/internal'
-import TableBasic from '../table/data-grid/TableBasic'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import CircularProgress from '@mui/material/CircularProgress'
@@ -33,11 +31,13 @@ const columns = [
     flex: 0.25,
     minWidth: 120,
     field: 'phone',
+    field: 'phone',
     headerName: 'Phone Number'
   },
   {
     flex: 0.15,
     minWidth: 120,
+    field: 'role_name',
     field: 'role_name',
     headerName: 'Role(s)'
   },
@@ -193,6 +193,26 @@ const UserTable = () => {
       })
     console.log(user, 'users')
   }, [])
+  const [loading, setLoading] = useState(false)
+  const [user, setUsers] = useState([])
+  useEffect(() => {
+    setLoading(true)
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API_URL}/get-users`)
+      .then(res => {
+        console.log(res?.data)
+        const user = res?.data['users']?.map(item => {
+          return { ...item, action: '' }
+        })
+        console.log(user, 'final users')
+        setUsers(user)
+        setLoading(false)
+      })
+      .catch(error => {
+        setLoading(false)
+      })
+    console.log(user, 'users')
+  }, [])
   return (
     <Box sx={{ height: 550 }}>
       {loading ? (
@@ -208,7 +228,7 @@ const UserTable = () => {
           <CircularProgress disableShrink sx={{ mt: 6 }} />{' '}
         </Box>
       ) : (
-        <TableBasic columns={columns} rows={user} hideFooter />
+        <DataGrid columns={columns} rows={user} />
       )}
     </Box>
   )
