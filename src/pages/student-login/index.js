86644bc218eb39f80af6fa873633506c0da9edc2
mihir07121 <mihir@jspinfotech.com>
2023-09-13 11:46:00 +0530

    add student-login-page

--- src/pages/student-login/index.js
+++ src/pages/student-login/index.js
@@ -1,84 +1,170 @@
 import React, { useState } from 'react'
-import { Box, Button, Container, Typography, TextField } from '@mui/material'
-import BlankLayout from 'src/@core/layouts/BlankLayout'
+import { Box, Button, Container, Typography, TextField, Link, IconButton } from '@mui/material'
+import InputAdornment from '@mui/material/InputAdornment'
+import VisibilityIcon from '@mui/icons-material/Visibility'
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
+// import BlankLayout from 'src/@core/layouts/BlankLayout'
+import BlankLayoutWithAppBar from 'src/@core/layouts/BlankLayoutWithAppBar'
 
 function StudentLogin() {
+  const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value)
+  }
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword)
+  }
+
   return (
     <Box
       sx={{
-       marginTop:"15%"
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+        // height: '100vh'
       }}
     >
-      <Container maxWidth='sm'>
-        <Box mt={5}>
-          <Typography variant='h4' align='center'>
-            Confirm Student Detail and Start Exam
-          </Typography>
-          <Box
+      {/* <Container> */}
+      <Box sx={{ maxWidth: '800px', width: '100%' }}>
+        <Box sx={{ marginBottom: '30px' }}>
+          <Typography
+            align='center'
             sx={{
-              padding: '20px',
-              background: '#FFF',
-              boxShadow: '0px 4px 44px 0px rgba(0, 0, 0, 0.10)'
+              fontSize: '21px',
+              fontWeight: 700
             }}
           >
-            <Box
+            Log In
+          </Typography>
+          <Typography
+            align='center'
+            sx={{
+              fontSize: '15px',
+              fontWeight: 400,
+              color: '#7D7D7D'
+            }}
+          >
+            Please sign in to your account.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            padding: '20px',
+            background: '#FFF',
+            boxShadow: '0px 4px 44px 0px rgba(0, 0, 0, 0.10)'
+          }}
+        >
+          <Box
+            sx={{
+              maxWidth: '100%',
+              padding: '10px'
+            }}
+          >
+            <Typography
               sx={{
-                maxWidth: '100%',
-                padding: '10px'
+                fontSize: '16px',
+                fontWeight: 400,
+                padding: '5px'
               }}
             >
-              <Typography
-                sx={{
-                  fontSize: '16px',
-                  fontWeight: 400,
-                  padding: '5px'
-                }}
-              >
-                Student Name
-              </Typography>
-              <TextField fullWidth label='Student Name' />
-            </Box>
+              User Id
+            </Typography>
+            <TextField
+              fullWidth
+              // label='User Id'
+            />
+          </Box>
 
-            <Box
+          <Box
+            sx={{
+              maxWidth: '100%',
+              padding: '10px'
+            }}
+          >
+            <Typography
               sx={{
-                maxWidth: '100%',
-                padding: '10px'
+                fontSize: '16px',
+                fontWeight: 400,
+                padding: '5px'
               }}
             >
-              <Typography
+              Passcode
+            </Typography>
+            <TextField
+              type={showPassword ? 'text' : 'password'} // Toggle between 'text' and 'password' for visibility
+              variant='outlined'
+              fullWidth
+              value={password}
+              onChange={handlePasswordChange}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position='end'>
+                    <IconButton onClick={handleTogglePasswordVisibility} edge='end'>
+                      {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                    </IconButton>
+                  </InputAdornment>
+                )
+              }}
+            />
+            {/* <Typography
                 sx={{
                   fontSize: '16px',
                   fontWeight: 400,
                   padding: '5px'
                 }}
               >
-                Student ID
+                Passcode
               </Typography>
-              <TextField fullWidth label='Student ID' />
-            </Box>
-            <Box
+              <TextField fullWidth label='Passcode' /> */}
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '10px'
+            }}
+          >
+            <Button
+              variant='contained'
               sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                padding: '10px'
+                border: 'none'
               }}
             >
+              Login
+            </Button>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '10px'
+            }}
+          >
+            <Link to='/your-link-path'>
               <Button
-                variant='contained'
+                size='medium'
                 sx={{
-                  border: 'none'
+                  textDecoration: 'underline',
+                  color: '#0C80BF'
                 }}
               >
-                Start Exam
+                Click here
               </Button>
-            </Box>
+            </Link>
+            {/* <Button size='medium'>Click here</Button> */}
           </Box>
         </Box>
-      </Container>
+      </Box>
+      {/* </Container> */}
     </Box>
   )
 }
-StudentLogin.getLayout = page => <BlankLayout>{page}</BlankLayout>
+// StudentLogin.getLayout = page => <BlankLayout>{page}</BlankLayout>
+StudentLogin.getLayout = page => <BlankLayoutWithAppBar>{page}</BlankLayoutWithAppBar>
+
 StudentLogin.guestGuard = false
 
 export default StudentLogin
